5e2bfee64be55f3a2e686520f711a2721fbf9da6
RazorzMcLazorz <young.nathaniel95@gmail.com>
2020-04-15 22:44:56 -0600

    grid builder

--- src/pages/kingdom.js
+++ src/pages/kingdom.js
@@ -5,13 +5,49 @@ import Background from '../components/backgroundImage'
 
 class Kingdom extends Component {
 
+  state = {
+    mapSize : {
+      'small' : 10
+    }
+  }
+
+  createArray(num) {
+    let arr = new Array
+    for (let i = 0; i < num; i++) {
+      arr.push(i)
+    }
+    return arr
+  }
+
+  MapLoader(size) {
+    let arr = this.createArray(size)
+
+    return (
+      <div>
+        {
+          arr.map( row => 
+            <div style={{ display: 'flex' }}>
+              {
+                arr.map(col => 
+                <div style={{ backgroundColor: '#FF5733', minWidth: 30, height: 30, fontSize: 8}}>
+                  {row}{col}
+                </div>  
+                )
+              }
+            </div>
+          )
+        }
+      </div>
+    )
+  }
+
   render() {
     console.log(this.props)
     return (
       <div className='kingdom'>
         <Background/>
         <div className='body'>
-          
+          {this.MapLoader(this.state.mapSize['small'])}
         </div>
       </div>
     )
